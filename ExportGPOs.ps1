cc645881860348c77d449e285360e577c868563e
Matteo Zamori <matteo.zamori@outlook.com>
2022-05-10 13:02:41 +0200

    Update ExportGPOs.ps1

--- ExportGPOs.ps1
+++ ExportGPOs.ps1
@@ -1,4 +1,4 @@
-﻿param (
+param (
     [Parameter(Mandatory = $true)]
     [System.String]
     $domainame,
@@ -11,18 +11,28 @@
 Clear-Host
 Set-Location -Path $PSScriptRoot
 
+#WhatIf
+$whatifpreference = $false
+
+#Log
+$date = Get-Date -Format "yyyy-MM-dd"
+$t=$MyInvocation.MyCommand.Name.Replace('.ps1','')
+$LogFilePath = $location + "\Logs" + "\${t}_${date}.log"
+
+Start-Transcript -Path "${LogFilePath}" | Out-Null
+
 #Create Folder Structure
 Write-Host 'Create Folders Structure' -ForegroundColor White
-New-Item ($location + "\GPOBackup") -ItemType Directory -Force | Out-Null
-New-Item ($location + "\GPODetailed") -ItemType Directory -Force | Out-Null
-New-Item ($location + "\GPOPolicyRules") -ItemType Directory -Force | Out-Null
-New-Item ($location + "\GPOsOutdated") -ItemType Directory -Force | Out-Null
-New-Item ($location + "\GPOSummary") -ItemType Directory -Force | Out-Null
+New-Item ($location + "\GPOResults\GPOBackup") -ItemType Directory -Force | Out-Null
+New-Item ($location + "\GPOResults\GPODetailed") -ItemType Directory -Force | Out-Null
+New-Item ($location + "\GPOResults\GPOPolicyRules") -ItemType Directory -Force | Out-Null
+New-Item ($location + "\GPOResults\GPOsOutdated") -ItemType Directory -Force | Out-Null
+New-Item ($location + "\GPOResults\GPOSummary") -ItemType Directory -Force | Out-Null
 
 #Get all GPOs Disabled or Partially Disabled (User or Computer object level)
 Write-Host 'Create Report for Disabled or Partially Disabled GPOs' -ForegroundColor Red
 $DisabledGPOs = Get-GPO -All -Domain $domainame | Where-Object {($_.GpoStatus -match 'disabled') -or ($_.GpoStatus -match 'AllSettingsDisabled')}
-$DisabledGPOs | Export-Csv -LiteralPath ($location + "\GPOsOutdated\" + "DisabledGPOSummary.csv")
+$DisabledGPOs | Export-Csv -LiteralPath ($location + "\GPOResults\GPOsOutdated\" + "DisabledGPOSummary.csv")
 
 #Get all GPOs in the domain
 $AllGPOs = Get-GPO -All -Domain $domainame | Where-Object {($_.GpoStatus -Notmatch 'disabled') -and ($_.GpoStatus -Notmatch 'AllSettingsDisabled')} 
@@ -32,18 +42,18 @@ foreach ($g in $AllGpos)
 {
     #Export GPO in XML format (not necessary but helps to pinpoint issue)
     Write-Host "Exporting $($g.DisplayName) in XML" -ForegroundColor Yellow
-    Get-GPOReport -ReportType Xml -Guid $g.Id -Path ($location + "\GPODetailed\" + $g.DisplayName + ".xml")
+    Get-GPOReport -ReportType Xml -Guid $g.Id -Path ($location + "\GPOResults\GPODetailed\" + $g.DisplayName + ".xml")
     
     #Backup GPOs
     Write-Host "Backup $($g.DisplayName)" -ForegroundColor Cyan
-    New-Item ($location + "\GPOBackup\" + $g.DisplayName) -ItemType Directory -Force | Out-Null
-    Backup-GPO -Name $g.DisplayName -Domain $domainame -Path ($location + "\GPOBackup\" + $g.DisplayName) | Out-Null
+    New-Item ($location + "\GPOResults\GPOBackup\" + $g.DisplayName) -ItemType Directory -Force | Out-Null
+    Backup-GPO -Name $g.DisplayName -Domain $domainame -Path ($location + "\GPOResults\GPOBackup\" + $g.DisplayName) | Out-Null
 
     #Automate PolicyRule Definition
     Write-Host "Generate PolicyRule Definition for $($g.DisplayName)" -ForegroundColor Magenta
     [string]$pathToGPO2PolicyRules = ($location + "\Tool\PolicyAnalyzer\GPO2PolicyRules.exe")
-    [Array]$arguments = ($location + "\GPOBackup\" + $g.DisplayName), ($location + "\GPOPolicyRules\" + $g.DisplayName + ".PolicyRules")
-    & $pathToGPO2PolicyRules $arguments 2> $null | Out-Null
+    [Array]$arguments = ($location + "\GPOResults\GPOBackup\" + $g.DisplayName), ($location + "\GPOResults\GPOPolicyRules\" + $g.DisplayName + ".PolicyRules")
+    & $pathToGPO2PolicyRules $arguments 2>&1 | ForEach-Object{ "$_" } | Out-Null
 
     [xml]$Gpo = Get-GPOReport -ReportType Xml -Guid $g.Id
     
@@ -57,15 +67,19 @@ foreach ($g in $AllGpos)
         {
             Write-Host "$($Gpo.GPO.Name) saved to CSV file"
             $ExportCSV = New-Object PSObject
-            $ExportCSV | Add-Member -MemberType NoteProperty -name 'Name' -value $Gpo.GPO.Name
+            $ExportCSV | Add-Member -MemberType NoteProperty -name 'GPO Name' -value $Gpo.GPO.Name
             $ExportCSV | Add-Member -MemberType NoteProperty -name 'Comp-Ad' -value $Gpo.GPO.Computer.VersionDirectory
             $ExportCSV | Add-Member -MemberType NoteProperty -name 'Comp-Sys' -value $Gpo.GPO.Computer.VersionSysvol
             $ExportCSV | Add-Member -MemberType NoteProperty -name 'Comp Enabled' -value $Gpo.GPO.Computer.Enabled
             $ExportCSV | Add-Member -MemberType NoteProperty -name 'User-Ad' -value $Gpo.GPO.User.VersionDirectory
             $ExportCSV | Add-Member -MemberType NoteProperty -name 'User-Sys' -value $Gpo.GPO.User.VersionSysvol
             $ExportCSV | Add-Member -MemberType NoteProperty -name 'User Enabled' -value $Gpo.GPO.User.Enabled
-            $ExportCSV | Add-Member -MemberType NoteProperty -name 'Link' -value $Gpo.GPO.LinksTo.SOMPath
-            $ExportCSV | Add-Member -MemberType NoteProperty -name 'Link Enabled' -value $Gpo.GPO.LinksTo.SOMPath
-            $ExportCSV | Export-CSV ($location+"\GPOSummary\"+"AllGPOSummary.csv") -Encoding UTF8 -Delimiter ";" -Force -NoTypeInformation -Append
+            $ExportCSV | Add-Member -MemberType NoteProperty -name 'Link Name' -value $Gpo.GPO.LinksTo.SOMName
+            $ExportCSV | Add-Member -MemberType NoteProperty -name 'Link Path' -value $Gpo.GPO.LinksTo.SOMPath
+            $ExportCSV | Add-Member -MemberType NoteProperty -name 'Link Enabled' -value $Gpo.GPO.LinksTo.Enabled
+            $ExportCSV | Add-Member -MemberType NoteProperty -name 'Link Override' -value $Gpo.GPO.LinksTo.NoOverride
+            $ExportCSV | Export-CSV ($location+"\GPOResults\GPOSummary\"+"AllGPOSummary.csv") -Encoding UTF8 -Delimiter ";" -Force -NoTypeInformation -Append
         }
 }
+
+Stop-Transcript | Out-Null
